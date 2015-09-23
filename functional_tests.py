3966ba002458cb0039f7c216c0b11930d3e26f9c
Sana Javed <sanajaved7@gmail.com>
2015-09-23 18:00:04 -0400

    Home page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -30,7 +30,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
 
         #Add more tests to prompt user to input another item
         self.fail('Finish the rest of the test!')
