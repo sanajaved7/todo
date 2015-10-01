2dc865b6ade72f3553f072c3e4065ef3e8f7e2dd
Sana Javed <sanajaved7@gmail.com>
2015-09-30 22:26:24 -0400

    Refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #User heard about new app and visits homepage
         self.browser.get('http://localhost:8000')
@@ -25,28 +30,23 @@ class NewVisitorTest(unittest.TestCase):
         #User enters 'buy peacock feathers'
         inputbox.send_keys('Buy peacock feathers')
 
-        #When user hits enter, now page lists 'buy peacock feathers' in a to-do list table
+        #When user hits enter, now page lists '1: Buy peacock feathers' in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-
-       #There is a text box asking the user to add another item. User enters 'Use peacock feathers to make a fly'
+        #There is still a text box asking the user to add another item. User enters 'Use peacock feathers to make a fly'
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-       #The page updates again and now both items appear on the to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        #The page updates again and now both items appear on the to-do list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #User wants to know if site will save her list and finds that the site has a unique URL for her and there is some explanatory text there.
 
         #User visits her unique URL and finds her list is still there
-        self.fail('Finish the rest of the test!')
+        #self.fail('Finish the rest of the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
